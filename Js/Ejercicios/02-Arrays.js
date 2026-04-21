9b7c36959e0230e4382ddd3289418a8fcac00882
Pablo SC <dev.pablosc@gmail.com>
2026-04-21 16:14:55 -0400

    practica arryas modificada

--- Js/Ejercicios/02-Arrays.js
+++ Js/Ejercicios/02-Arrays.js
@@ -4,4 +4,7 @@
 
 // 3. Elimina el que se encuentra en tercera posición
 
-// 4. Comprueba si el mes número 5 existe en el map e imprime su valor
+// 4. Comprueba si el mes número 5 existe en el arrays e imprime su valor
+
+// 5. crear un array con 10 datos de cualquier tipo y recorre el array 
+// con un bucle for hasta la mitad
